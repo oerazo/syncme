2d0e7d05b7aa9005fbe30a72e4761ee848f3a63b
Orlando Erazo <erazomartinez@gmail.com>
2019-11-12 21:33:32 +1100

    Validate if there are additional new files, add etag validation, also warning on content changes

--- bin/syncme.sh
+++ bin/syncme.sh
@@ -47,21 +47,33 @@ do
     path=$(dirname "${file}")
     #Get base
     base=$(basename "${file}")
-    #Sync one file at the time to run some validations
+    #check if local file exits in s3
+    inS3=$(aws s3 ls s3://$bucket/${path#/}/$base)
+    [ ! -z "$inS3" ] || echo "Warning, $file found in filer but not in s3, continuing"
+    # Perform some validations
     result=$(aws s3 sync $path s3://$bucket/${path#/} --exclude='*' --include=$base --output text)
     if [ -z "$result" ]; then
       #Get eTag for this object in s3
-      eTag=$(aws s3api head-object --bucket $bucket --key ${path#/}/$base --query ETag --output text)
-      #echo $eTag
+      eTag=$(aws s3api head-object --bucket $bucket --key ${path#/}/$base --query ETag --output text) || die " (fail) unable to sync file $file"
+      #JMESPath returns an escaped json result, as there is no raw option in JMESPath and I dont want to use JQ just for this then Im just removing the quotes
+      #This tech debt has been brought to you by me using bash :)
+      eTag=${eTag#\"}
+      eTag=${eTag%\"}
       #Calculate eTag on local file - no multipart required
-      filerETag="$(dd bs=1m count=1 if=$file 2>/dev/null | md5)"
-      #echo $filerETag
+      filerETag=$(dd bs=1m count=1 if=$file 2>/dev/null | md5)
+      #check file integrity MD5 eTag
       if [ $eTag == $filerETag ]; then
-        echo "Checksum success"
+        echo "$file was found in filer and it is identical in s3 (this includes eTag validation)"
+      else
+        echo "Check $file as its integrity might have been compromised"
       fi
-      echo "$file was found in filer and it is identical in s3"
+
     else
-      echo $result
+      if [ ! -z "$inS3" ]; then
+        echo "Warning - the content of file $file has changed, continuing"
+      else
+        echo $result
+      fi
     fi
 
 done
